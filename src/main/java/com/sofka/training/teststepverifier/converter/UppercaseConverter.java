95c48510caff36bda0f5efe797362583f8d45ef8
Diego Martinez <juandiego337@hotmail.com>
2021-10-08 08:57:48 -0500

    Testing reactive streams with testPublisher

---
+++ src/main/java/com/sofka/training/teststepverifier/converter/UppercaseConverter.java
@@ -0,0 +1,18 @@
+package com.sofka.training.teststepverifier.converter;
+
+import reactor.core.publisher.Flux;
+
+public class UppercaseConverter {
+
+    private final Flux<String> source;
+
+    UppercaseConverter(Flux<String> source) {
+        this.source = source;
+    }
+
+    Flux<String> getUpperCase() {
+        return source
+                .map(String::toUpperCase);
+    }
+
+}
